6f938d1c29a53dc2c699faa08d079e7a10354374
Gert <111555089+GitGert@users.noreply.github.com>
2024-11-07 17:50:47 +0200

    first comit: added create-react-app frontend and Java springboot backend

---
+++ src/main/java/ee/gert/bowling_app/BowlingAppApplication.java
@@ -0,0 +1,13 @@
+package ee.gert.bowling_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlingAppApplication.class, args);
+	}
+
+}
